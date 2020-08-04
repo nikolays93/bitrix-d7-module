2de87c502971b2300e16513f0697556240d40032
NikolayS93 <nikolays93@ya.ru>
2020-08-04 10:50:13 +0400

    Add boilerplate component

---
+++ install/components/boilerplate.component/class.php
@@ -0,0 +1,43 @@
+<?php if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();
+
+use \Bitrix\Main\Localization\Loc;
+use \Bitrix\Main\Loader;
+
+class CustomBitrixComponent extends CBitrixComponent
+{
+    public $errors = array();
+
+    /**
+     * @param CBitrixComponent|null $component
+     * @throws Bitrix\Main\LoaderException
+     */
+    public function __construct($component = null)
+    {
+        parent::__construct($component);
+
+        Loader::includeModule('sale');
+        Loader::includeModule('catalog');
+        Loader::includeModule(boilerplate_module::MODULE_ID);
+    }
+
+    public function onIncludeComponentLang()
+    {
+        Loc::loadLanguageFile(__FILE__);
+    }
+
+    public function onPrepareComponentParams($arParams = array())
+    {
+        return $arParams;
+    }
+
+    public function executeComponent()
+    {
+        try {
+
+        } catch (Exception $exception) {
+            array_push($this->errors, new Error($exception->getMessage()));
+        }
+
+        $this->includeComponentTemplate();
+    }
+}
