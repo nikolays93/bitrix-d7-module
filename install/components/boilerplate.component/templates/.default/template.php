5990ccc00f710f5e7b4cedf34a2646a05cedccfb
Nick Sergeevich <nikolays93@ya.ru>
2021-02-20 10:14:53 +0400

    Ajax example

--- install/components/boilerplate.component/templates/.default/template.php
+++ install/components/boilerplate.component/templates/.default/template.php
@@ -9,3 +9,19 @@
 /** @var string $templateFile */
 /** @var string $templateFolder */
 /** @var string $componentPath */
+
+?>
+<script>
+    const componentName = 'boilerplate.component';
+    const actionName = 'test';
+
+    BX.ajax.runComponentAction(componentName, actionName, {
+        mode: 'ajax', // || class - обращаться к классу компонента или к ajax.php файлу
+        data: {
+            property_name1: 'testMessage1',
+            property_name2: 'testMessage2',
+        }
+    }).then(function (response) {
+        console.log(response);
+    });
+</script>
